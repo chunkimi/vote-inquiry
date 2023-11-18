6bc74f75469f689a54e033c42229ca0824396550
sfwang <fy800328@gmail.com>
2023-11-19 01:57:23 +0800

    fix(router): add name property

--- src/router/index.js
+++ src/router/index.js
@@ -14,30 +14,35 @@ const routes = [
     children: [
       {
         path: '',
-        component: HomeView
+        name: 'Home',
+        component: HomeView,
       },
       {
         // 當屆選情分析
         path: 'analysis',
-        component: AnalysisView
-        },
-        {
+        name: 'Analysis',
+        component: AnalysisView,
+      },
+      {
         // 歷屆選情－所有年份列表
         path: 'past-elections',
-        component: PastElectionsView
-        },
-        
-        {
+        name: 'PastElections',
+        component: PastElectionsView,
+      },
+
+      {
         // 歷屆選情－所選年份選情分析
         path: 'past-elections/:year',
-        component: PastAnalysisView
-        },
-        {
+        name: 'PastAnalysis',
+        component: PastAnalysisView,
+      },
+      {
         // 候選人名單
         path: 'candidate',
-        component: CandidateView
-        },
-    ]
+        name: 'Candidate',
+        component: CandidateView,
+      },
+    ],
   },
 ]
 
