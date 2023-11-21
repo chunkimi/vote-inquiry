3d2901454674ba6ee0557af796c11067a7ca4036
sfwang <fy800328@gmail.com>
2023-11-22 01:14:21 +0800

    feat: integrate firebase storage

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,7 @@
 <template>
   <h2>This page is Home (index)</h2>
   <i class="bi bi-0-circle"></i>
+  <div class="icon-vote-stamp"></div>
   <button type="button" class="btn btn-primary">Primary</button>
   <button type="button" class="btn btn-secondary">Secondary</button>
   <button type="button" class="btn btn-success">Success</button>
@@ -13,7 +14,18 @@
   <button type="button" class="btn btn-savannah">金色曠野同盟</button>
   <button type="button" class="btn btn-coast">蔚藍海岸陣線</button>
   <button type="button" class="btn btn-rainforest">鬱蔥雨林聯盟</button>
+  {{ data }}
 </template>
 
-<script>
+<script setup>
+import { useFirebaseStorage, useStorageFile } from 'vuefire'
+import { ref as storageRef } from 'firebase/storage'
+import { useFetch } from '@vueuse/core'
+
+const storage = useFirebaseStorage()
+const candidateFileRef = storageRef(storage, 'votes/2020/屏東縣.json')
+
+const { url: candidate_url } = useStorageFile(candidateFileRef)
+
+const { data } = useFetch(candidate_url, { refetch: true }).get().json()
 </script>
