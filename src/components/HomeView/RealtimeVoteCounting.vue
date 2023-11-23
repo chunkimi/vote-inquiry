ccf3966261a91e6f7465c766604add667eeadbae
sfwang <fy800328@gmail.com>
2023-11-24 01:12:47 +0800

    style(HomeView): create RealtimeVoteCounting Component

---
+++ src/components/HomeView/RealtimeVoteCounting.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="d-flex px-12 py-4 gap-8 align-items-center">
+    <p class="h3">{{ id }}號</p>
+    <img
+      :src="partyLogo"
+      class="rounded-circle"
+      alt="party-logo"
+      width="100"
+      height="100"
+    />
+    <img
+      :src="avatar"
+      class="rounded-circle"
+      alt="candidate-avatar"
+      width="100"
+      height="100"
+    />
+    <div class="d-grid gap-2">
+      <p class="h5">得票數：{{ count }}票</p>
+      <p class="h5">得票率：{{ percentage }}%</p>
+    </div>
+    <div
+      class="progress flex-grow-1"
+      role="progressbar"
+      aria-label="Voting Percentage"
+      aria-valuemin="0"
+      aria-valuemax="100"
+      :aria-valuenow="percentage"
+      :title="`${percentage}%`"
+    >
+      <div
+        ref="progressBarPercentage"
+        class="progress-bar"
+        :class="progressColor"
+        :style="{ width: `${percentage}%` }"
+      >
+        <span v-if="progressBarWidth > 30" ref="progressBarPercentage">
+          {{ percentage }}%
+        </span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref, computed } from 'vue'
+import { useElementSize } from '@vueuse/core'
+import partyMap from '@/data/party.json'
+
+const props = defineProps({
+  id: {
+    type: [String, Number],
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  party: {
+    type: String,
+    required: true,
+  },
+  partyLogo: {
+    type: String,
+    required: true,
+  },
+  avatar: {
+    type: String,
+    required: true,
+  },
+  count: {
+    type: Number,
+    required: true,
+  },
+  percentage: {
+    type: Number,
+    required: true,
+  },
+})
+
+const progressBarPercentage = ref(null)
+const { width: progressBarWidth } = useElementSize(progressBarPercentage)
+
+const progressColor = computed(() => {
+  const colorCode = partyMap.codeMap[props.party]
+  console.log(colorCode)
+  return colorCode ? `bg-${colorCode}` : ''
+})
+</script>
