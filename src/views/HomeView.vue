8a5e3fa645f0535d1d078fee7c080a802630ccb2
sfwang <fy800328@gmail.com>
2023-11-24 01:13:07 +0800

    style(HomeView): create HomeView Component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,45 +1,62 @@
 <template>
-  <h2>This page is Home (index)</h2>
-  <SearchBar v-model:city="city" v-model:district="district" year="2012" />
-  <i class="bi bi-0-circle"></i>
-  <div class="icon-vote-stamp"></div>
-  <button type="button" class="btn btn-primary">Primary</button>
-  <button type="button" class="btn btn-secondary">Secondary</button>
-  <button type="button" class="btn btn-success">Success</button>
-  <button type="button" class="btn btn-danger">Danger</button>
-  <button type="button" class="btn btn-warning">Warning</button>
-  <button type="button" class="btn btn-info">Info</button>
-  <button type="button" class="btn btn-light">Light</button>
-  <button type="button" class="btn btn-dark">Dark</button>
-  <button type="button" class="btn btn-link">Link</button>
-  <button type="button" class="btn btn-savannah">金色曠野同盟</button>
-  <button type="button" class="btn btn-coast">蔚藍海岸陣線</button>
-  <button type="button" class="btn btn-rainforest">鬱蔥雨林聯盟</button>
-  <ul>
-    <li v-for="(candidate, i) in candidateList" :key="i">
-      {{ candidate.name }} - {{ candidate.party }}
-    </li>
-  </ul>
+  <IconLabel text="開票地圖" icon="bi-geo-alt-fill" />
   <VoteMap :data="voteMapData" />
+  <IconLabel text="選票即時報" icon="bi-rss" />
+  <div class="d-grid gap-4">
+    <RealtimeVoteCounting
+      v-for="candidate in realtimeSummary"
+      :key="candidate.id"
+      :id="candidate.id"
+      :name="candidate.name"
+      :party="candidate.party"
+      :partyLogo="candidate.partyLogo"
+      :avatar="candidate.avatar"
+      :count="candidate.count"
+      :percentage="candidate.percentage"
+    />
+  </div>
 </template>
 
 <script setup>
 import { ref, computed } from 'vue'
-import { useFirebaseStorage, useStorageFile } from 'vuefire'
-import { ref as storageRef } from 'firebase/storage'
-import { useFetch } from '@vueuse/core'
+// import { useFirebaseStorage, useStorageFile } from 'vuefire'
+// import { ref as storageRef } from 'firebase/storage'
+// import { useFetch } from '@vueuse/core'
 import VoteMap from '@/components/common/VoteMap.vue'
-import SearchBar from '@/components/common/SearchBar.vue'
+import RealtimeVoteCounting from '@/components/HomeView/RealtimeVoteCounting.vue'
+import IconLabel from '@/components/common/IconLabel.vue'
+import candidate from '@/data/candidate.json'
 
-const storage = useFirebaseStorage()
-const candidateFileRef = storageRef(storage, 'candidate/candidate.json')
+const currentElectionYear = '2020'
 
-const { url: candidate_url } = useStorageFile(candidateFileRef)
-
-const { data } = useFetch(candidate_url, { refetch: true }).get().json()
-const candidateList = computed(() =>
-  (data.value || []).filter(({ election_year }) => election_year === '2020'),
+const realtimeSummary = computed(() =>
+  candidate
+    .filter(
+      ({ election_year, role }) =>
+        election_year === currentElectionYear && role === 0,
+    )
+    .map(({ candidate_id, name, party, party_logo_url, avatar_url }) => {
+      return {
+        id: candidate_id,
+        name,
+        party,
+        partyLogo: new URL(`../${party_logo_url}`, import.meta.url).href,
+        avatar: new URL(`../${avatar_url}`, import.meta.url).href,
+        count: 123456,
+        percentage: 10,
+      }
+    }),
 )
+
+// const storage = useFirebaseStorage()
+// const candidateFileRef = storageRef(storage, 'candidate/candidate.json')
+
+// const { url: candidate_url } = useStorageFile(candidateFileRef)
+
+// const { data } = useFetch(candidate_url, { refetch: true }).get().json()
+// const candidateList = computed(() =>
+//   (data.value || []).filter(({ election_year }) => election_year === '2020'),
+// )
 
 const voteMapData = ref([
   { city: '臺北市', party: '金色曠野同盟', count: 213 },
@@ -48,7 +65,4 @@ const voteMapData = ref([
   { city: '嘉義縣', party: '鬱蔥雨林聯盟', count: 12334 },
   { city: '彰化縣', party: '蔚藍海岸陣線', count: 1233 },
 ])
-
-const city = ref('')
-const district = ref('')
 </script>
