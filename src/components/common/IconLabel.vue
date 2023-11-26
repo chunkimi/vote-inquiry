5accd29fe4678474a69b54984c2eef028b6b4fb1
sfwang <fy800328@gmail.com>
2023-11-26 19:07:58 +0800

    feat(IconLabel): add vote-stamp icon

--- src/components/common/IconLabel.vue
+++ src/components/common/IconLabel.vue
@@ -1,7 +1,24 @@
+<style lang="scss">
+.icon-vote-stamp {
+  width: 24px;
+  height: 24px;
+  background-image: url('src/assets/icons/vote-stamp.svg');
+  background-size: cover;
+  vertical-align: -3px;
+}
+</style>
+
 <template>
   <h4 class="h4">
+    <span
+      v-if="icon === 'vote-stamp'"
+      class="d-inline-block icon-vote-stamp"
+    ></span>
     <i class="bi" :class="icon"></i>
     {{ text }}
+    <small v-if="source" class="fs-7 fw-normal text-gray-500 d-block px-8 py-2">
+      {{ `※資料來源：${source}` }}
+    </small>
   </h4>
 </template>
 
@@ -14,6 +31,9 @@ defineProps({
   icon: {
     type: String,
     default: 'bi-geo-alt-fill',
+  },
+  source: {
+    type: String,
   },
 })
 </script>
