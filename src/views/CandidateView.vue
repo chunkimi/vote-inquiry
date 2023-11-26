1a38e73ed67e69d93b5b27ce167ca293966cf303
kathyhu91 <huchiachun@gmail.com>
2023-11-27 01:44:27 +0800

    separate TermMenu into a component

--- src/views/CandidateView.vue
+++ src/views/CandidateView.vue
@@ -1,46 +1,11 @@
-<style lang="scss">
-@import '@/styles/main.scss';
-
-.horizontalList {
-  &__item {
-    padding: 10px 16px;
-    font-size: 1rem;
-    font-weight: 500;
-    color: $warning;
-    cursor: pointer;
-    border: 1px solid $warning;
-    &:first-child {
-      border-right: none;
-      border-radius: 4px 0px 0px 4px;
-    }
-    &:last-child {
-      border-left: none;
-      border-radius: 0px 4px 4px 0px;
-    }
-    &:hover,
-    &:active,
-    &.active {
-      color: $white;
-      background-color: $warning;
-    }
-  }
-}
-</style>
 <template>
   <h1 class="h2 fw-bold text-center">歷屆候選公僕名單</h1>
-
-  <ul class="list-unstyled d-flex justify-content-center">
-    <li
-      class="horizontalList__item"
-      v-for="year in years"
-      :key="year"
-      :class="{ active: year === curYear }"
-    >
-      <span v-if="year === curYear"> {{ year }}</span>
-      <span v-else @click.prevent="getYearData(year)"> {{ year }}</span>
-    </li>
-  </ul>
   <div v-if="!isLoading">
+    <TermMenu
+      :election-Years="years"
+      :specify-Year="curYear"
+      @change-Year="switchMenu"
+    ></TermMenu>
     <CandidateInfo
       :specify-Year="curYear"
       :election-Parties="parties"
@@ -55,17 +20,26 @@ import { ref, onMounted } from 'vue'
 import candidateData from '@/data/candidate.json'
 import { filterCandidateDataType } from '@/utils/candidateFilter'
 import CandidateInfo from '@/components/CandidateInfo.vue'
+import TermMenu from '@/components/common/TermMenu.vue'
 
 // 資料統計：年份、政黨
-const years = ref(filterCandidateDataType(candidateData, 'election_year'))
-const parties = ref(filterCandidateDataType(candidateData, 'party'))
+const years = ref(null)
+const parties = ref(null)
 // 指定年份數據
 const curYear = ref(null)
 const curData = ref(null)
 const isLoading = ref(true)
-
 // methods
-const getYearData = (year, candidate) => {
+const getYearData = (data) => {
+  years.value = filterCandidateDataType(data, 'election_year')
+}
+
+const getPartyData = (data) => {
+  parties.value = filterCandidateDataType(data, 'party')
+}
+
+const getCandidateInfo = (year, candidate) => {
+  isLoading.value = true
   curYear.value = year
   curData.value = candidate.filter(
     (item) => item.election_year == curYear.value,
@@ -73,7 +47,21 @@ const getYearData = (year, candidate) => {
   isLoading.value = false
 }
 
+const switchMenu = (year) => {
+  curYear.value = year
+  getCandidateInfo(year, candidateData)
+}
+
+const init = () => {
+  getYearData(candidateData)
+  getPartyData(candidateData)
+  if (years.value.length > 0 && parties.value.length > 0) {
+    getCandidateInfo('2020', candidateData)
+    isLoading.value = false
+  }
+}
+
 onMounted(() => {
-  getYearData('2020', candidateData)
+  init()
 })
 </script>
