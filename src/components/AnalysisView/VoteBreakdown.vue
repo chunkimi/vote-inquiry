3a9878347dfea0f9fc392fb930275620f62f387c
sfwang <fy800328@gmail.com>
2023-11-28 02:40:03 +0800

    feat(AnalysisView): create VoteBreakdown component

---
+++ src/components/AnalysisView/VoteBreakdown.vue
@@ -0,0 +1,22 @@
+<style>
+.vote-breakdown-wrapper {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+}
+</style>
+<template>
+  <div class="vote-breakdown-wrapper d-grid gap-8">
+    <AnalysisCard v-for="(vote, i) in votes" :key="i" :vote="vote" />
+  </div>
+</template>
+
+<script setup>
+import AnalysisCard from '@/components/AnalysisView/AnalysisCard.vue'
+
+defineProps({
+  votes: {
+    type: Array,
+    required: true,
+  },
+})
+</script>
