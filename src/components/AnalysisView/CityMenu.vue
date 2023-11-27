24ad3452f425b673954fa9ec16648926b3754763
sfwang <fy800328@gmail.com>
2023-11-28 02:40:27 +0800

    feat(AnalysisView): create CityMenu component

---
+++ src/components/AnalysisView/CityMenu.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="accordion" id="accordionExample">
+    <div
+      v-for="(cities, region) in regionData"
+      :key="region"
+      class="accordion-item"
+    >
+      <h2 class="accordion-header" :id="`header-${region}`">
+        <button
+          class="accordion-button collapsed"
+          type="button"
+          data-bs-toggle="collapse"
+          :data-bs-target="`#collapse-${region}`"
+          aria-expanded="false"
+          :aria-controls="`collapse-${region}`"
+        >
+          {{ region }}
+        </button>
+      </h2>
+      <div
+        :id="`collapse-${region}`"
+        class="accordion-collapse collapse"
+        :aria-labelledby="`header-${region}`"
+        data-bs-parent="#accordionExample"
+      >
+        <div class="accordion-body d-flex flex-wrap gap-2">
+          <span
+            v-for="(cityName, i) in cities"
+            :key="i"
+            class="badge bg-primary"
+            @click="city = cityName"
+          >
+            {{ cityName }}
+          </span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { computed } from 'vue'
+import { storeToRefs } from 'pinia'
+import { useCurrentElectionStore } from '@/stores/currentElectionStore'
+import { useFetch } from '@vueuse/core'
+import city_id_section_map from '@/data/city_id_section_map.json'
+import city_id_map from '@/data/city_id_map.json'
+
+const currentElectionStore = useCurrentElectionStore()
+const { currentElectionYear, city } = storeToRefs(currentElectionStore)
+
+const path = computed(
+  () =>
+    new URL(
+      `../../data/administrative_area/${currentElectionYear.value}.json`,
+      import.meta.url,
+    ).href,
+)
+const { data } = useFetch(path, { refetch: true }).get().json()
+
+const regions = ['北部', '中部', '南部', '東部', '離島']
+const cityList = computed(() => Object.keys(data.value || {}))
+const regionData = computed(() => {
+  return regions.reduce((res, region) => {
+    const cites = cityList.value.filter((city) => {
+      const cityId = city_id_map[city]
+      const regionName = city_id_section_map[cityId]
+      return regionName === region
+    })
+
+    if (cites.length === 0) return res
+
+    res[region] ??= []
+    res[region].push(...cites)
+
+    return res
+  }, {})
+})
+</script>
+
+<!-- "北部": ["A", "F", "C", "H", "J", "O"],
+"中部": ["K", "B", "L", "N", "M", "P"],
+"南部": ["Q", "I", "D", "R", "E", "S", "T"],
+"東部": ["G", "U", "V"],
+"離島": ["W", "Z", "X"], -->
