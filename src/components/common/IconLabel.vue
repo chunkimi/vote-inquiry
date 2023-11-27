24318d7ba747649726a10ed1d7c873828293c9a2
sfwang <fy800328@gmail.com>
2023-11-28 00:03:03 +0800

    style(IconLabel): remove default icon value

--- src/components/common/IconLabel.vue
+++ src/components/common/IconLabel.vue
@@ -14,7 +14,7 @@
       v-if="icon === 'vote-stamp'"
       class="d-inline-block icon-vote-stamp"
     ></span>
-    <i class="bi" :class="icon"></i>
+    <i v-else-if="icon" class="bi" :class="icon"></i>
     {{ text }}
     <small v-if="source" class="fs-7 fw-normal text-gray-500 d-block px-8 py-2">
       {{ `※資料來源：${source}` }}
@@ -30,7 +30,6 @@ defineProps({
   },
   icon: {
     type: String,
-    default: 'bi-geo-alt-fill',
   },
   source: {
     type: String,
