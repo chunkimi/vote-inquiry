8a048bd84a9888cbc0a765ec6e684cc752b9a65d
kathyhu91 <huchiachun@gmail.com>
2024-01-02 14:03:01 +0800

    feat(voterTurnout): add summary text

--- src/components/PastAnal/VoterTurnout.vue
+++ src/components/PastAnal/VoterTurnout.vue
@@ -1,24 +1,17 @@
-<style scoped>
-lh-1point75 {
-  line-height: 1.75rem;
-}
-</style>
 <template>
   <h4 class="h4 mb-4">{{ curStatus }}歷屆投票率變化</h4>
   <div class="row">
-    <div class="col-md-7">
+    <div class="col-md-8">
       <PastAnalLineChart
         id="voter-turnout"
         :data="lineData"
       ></PastAnalLineChart>
     </div>
-    <div class="col-md-5">
-      <!-- <p class="lh-1point75">
-        本屆與{{ voterTurnoutComparison.benchmark }}投票率比較
+    <div class="col-md-4">
+      <p class="mb-1">本屆與{{ SummaryText.benchmark }}投票率比較</p>
+      <p class="h2" :class="`text-${SummaryText.textStyle}`">
+        {{ `${SummaryText.text}${SummaryText.num}%` }}
       </p>
-      <p class="h2" :class="voterTurnoutComparison.textStyle">
-        {{ `${voterTurnoutComparison.text}${voterTurnoutComparison.num}%` }}
-      </p> -->
     </div>
   </div>
 </template>
@@ -46,5 +39,31 @@ const lineData = computed(() => {
     labels,
     data,
   }
+})
+
+const SummaryText = computed(() => {
+  const allYear = Object.keys(props.originAreaVotes)
+    .map((str) => str.match(/\d+/)[0])
+    .sort((a, b) => b - a)
+  const curVoterTurnout =
+    props.originAreaVotes[`vote${curYear.value}`]['投票率']
+  const curVoterTurnoutIndex = allYear.indexOf(curYear.value)
+  const isOldestYear =
+    curVoterTurnoutIndex === allYear.length - 1 ? true : false
+  const compYear = isOldestYear
+    ? allYear[curVoterTurnoutIndex - 1]
+    : allYear[curVoterTurnoutIndex + 1]
+  const compVoterTurnout = props.originAreaVotes[`vote${compYear}`]['投票率']
+  const isIncrease = curVoterTurnout > compVoterTurnout ? true : false
+
+  const result = {
+    benchmark: isOldestYear ? '下屆' : '上屆',
+    text: isIncrease ? '成長' : '下降',
+    textStyle: isIncrease ? 'success' : 'danger',
+    num: isIncrease
+      ? (parseFloat(curVoterTurnout) - parseFloat(compVoterTurnout)).toFixed(2)
+      : (parseFloat(compVoterTurnout) - parseFloat(curVoterTurnout)).toFixed(2),
+  }
+  return result
 })
 </script>
