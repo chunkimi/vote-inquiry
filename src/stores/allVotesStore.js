d035bb676f7c8660416b4003eadd554eaa4f1057
kathyhu91 <huchiachun@gmail.com>
2024-01-05 23:03:40 +0800

    feat(allVotesStore) : function getAllVotes draft

--- src/stores/allVotesStore.js
+++ src/stores/allVotesStore.js
@@ -13,6 +13,7 @@ export const useAllVotesStore = defineStore('pastElectionStore', () => {
   const specifyDistrict = ref('')
   const allVotes = ref({})
 
+
   const viewStatus = computed(() => {
     if (!specifyCity.value && !specifyDistrict.value) {
       return `全國`
@@ -30,15 +31,54 @@ export const useAllVotesStore = defineStore('pastElectionStore', () => {
     immediate: true,
   })
 
+  //todo:取得指定地區，歷屆選舉資料，處理桃園例外情況
   function getAllVotes() {
     allYears.forEach((yearIndex) => {
         const storage = useFirebaseStorage()
         const votesFileRef = computed(() => {
-          const path = combineVotePath(
-            yearIndex,
-            specifyCity.value,
-            specifyCity.value,
-          )
+            let  path =""
+          if(specifyCity.value.includes("桃園")) {
+            if(specifyCity.value === "桃園縣") {
+                if(yearIndex != "2012" ) {
+                    const  substituteDistrict = specifyDistrict.value.replace(/市$|鄉$|區$|鎮$/, '區')
+                    path = combineVotePath(
+                        yearIndex,
+                        "桃園市",
+                        specifyCity.value,
+                        substituteDistrict
+                      )
+                } else {
+                    path = combineVotePath(
+                        yearIndex,
+                        specifyCity.value,
+                        specifyDistrict.value,
+                      ) 
+                }
+            } else {
+                if(yearIndex != "2012" ) {
+                    path = combineVotePath(
+                        yearIndex,
+                        specifyCity.value,
+                        specifyDistrict.value,
+                      )
+                } else {
+                      const  substituteDistrict = ""
+                      //從"區"逆轉回為"鄉鎮市區"，要另外做對照表District_id
+                      path = combineVotePath(
+                          yearIndex,
+                          "桃園市",
+                          specifyCity.value,
+                          substituteDistrict
+                        )
+                }  
+            }
+          } else {
+            path = combineVotePath(
+                yearIndex,
+                specifyCity.value,
+                specifyDistrict.value,
+              )
+          }
           return storageRef(storage, path)
         })
         const { url } = useStorageFileUrl(votesFileRef)
