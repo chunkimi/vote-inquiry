cebac350ae92aa2e88d9c7f9a169a569d3946b0f
kathyhu91 <huchiachun@gmail.com>
2024-01-24 22:50:13 +0800

    feat(useVotesData) : add useVotesData for remote API abstraction

--- src/stores/pastVotesStore.js
+++ src/stores/pastVotesStore.js
@@ -38,35 +38,66 @@ export const usePastVotesStore = defineStore('pastElectionStore', () => {
     filterSameSession(curYear.value, candidate),
   )
 
-  const votes = ref([])
-  const storage = useFirebaseStorage()
-  const votesFileRef = computed(() => {
-    if (!curYear.value) return
-    const path = combineVotePath(
-      curYear.value,
-      curCity.value,
-      curDistrict.value,
-    )
-    return storageRef(storage, path)
-  })
-  const { url } = useStorageFileUrl(votesFileRef)
+  // const votes = ref([])
+  // const storage = useFirebaseStorage()
+  // const votesFileRef = computed(() => {
+  //   if (!curYear.value) return
+  //   const path = combineVotePath(
+  //     curYear.value,
+  //     curCity.value,
+  //     curDistrict.value,
+  //   )
+  //   return storageRef(storage, path)
+  // })
+  // const { url } = useStorageFileUrl(votesFileRef)
 
-  const { data, onFetchResponse } = useFetch(url, {
-    refetch: true,
-    beforeFetch({ url, options, cancel }) {
-      // avoid fetch when url is empty
-      if (!url) cancel()
-      return options
-    },
-  })
-    .get()
-    .json()
+  // const { data, onFetchResponse } = useFetch(url, {
+  //   refetch: true,
+  //   beforeFetch({ url, options, cancel }) {
+  //     // avoid fetch when url is empty
+  //     if (!url) cancel()
+  //     return options
+  //   },
+  // })
+  //   .get()
+  //   .json()
 
-  onFetchResponse(() => {
-    votes.value = data.value
-  })
-
+  // onFetchResponse(() => {
+  //   votes.value = data.value
+  // })
+  const { votes } = useVotesData(curYear, curCity, curDistrict)
+  function useVotesData(year, city, district) {
+    const storage = useFirebaseStorage()
+    const votesFileRef = computed(() => {
+      if (!year.value) return
+      const path = combineVotePath(
+        year.value,
+        city.value,
+        district.value,
+      )
+      return storageRef(storage, path)
+    })
+    const { url } = useStorageFileUrl(votesFileRef)
   
+    const { data, onFetchResponse } = useFetch(url, {
+      refetch: true,
+      beforeFetch({ url, options, cancel }) {
+        // 避免在 url 為空時進行 fetch
+        if (!url) cancel()
+        return options
+      },
+    })
+      .get()
+      .json()
+  
+    const votes = ref([])
+  
+    onFetchResponse(() => {
+      votes.value = data.value
+    })
+  
+    return { votes }
+  }
 
   watch(
     curYear,
