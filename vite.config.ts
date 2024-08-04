299be79593f1b319dd7b97baa9aef87cdc3dfd69
fiona <fy800328@gmail.com>
2024-08-04 19:00:34 +0800

    build: upgrade vite version

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'node:url'
-import { resolve, dirname } from 'node:path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,11 +8,10 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
-      '~bootstrap': resolve(
-        dirname(fileURLToPath(import.meta.url)),
-        'node_modules/bootstrap',
-      ),
     },
   },
   base: '/vote-inquiry',
+  optimizeDeps: {
+    include: ['bootstrap'],
+  },
 })
